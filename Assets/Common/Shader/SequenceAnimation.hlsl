a5c307c37769dc34cd1e39ce688f44bc3ce3dcea
Krus_ROG_3060 <selfishkrus@github.com>
2024-03-19 22:59:42 +0800

    <shader> sequence animation

---
+++ Assets/Common/Shader/SequenceAnimation.hlsl
@@ -0,0 +1,15 @@
+#ifndef SEQUENCE_ANIMATION_INCLUDED
+#define SEQUENCE_ANIMATION_INCLUDED
+
+    void SequenceAnimationUV_float(float uNum, float vNum, float time, float speed, float2 uv, out float2 uv_sequence)
+    {
+        float intTime = floor(time * speed);
+        float row = floor(intTime / uNum);
+        float column = intTime - row * vNum;
+
+        uv_sequence = uv + float2(column, -row);
+        uv_sequence.x /= uNum;
+        uv_sequence.y /= vNum;
+    }
+
+#endif 
