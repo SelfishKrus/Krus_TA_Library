844a83055f5b4c7a09ab20519a23ab9cf8dc5d23
Krus_ROG_3060 <selfishkrus@github.com>
2024-04-19 19:16:19 +0800

    sort out

--- Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/PathUtils.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class PathUtils
-    {
-        public const string SubstanceRootPath = "Assets/Adobe/Substance3DForUnity/";
-
-        public const bool UsingPackageManager = true;
-    }
-}
--- Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class Version
-    {
-        public const string PluginVersion = "3.10.1-0-gbc95ebc";
-        public const string EngineVersion = "9.0.0 537e2e31";
-        public const int ImporterVersion = 3101;
-    }
-}
